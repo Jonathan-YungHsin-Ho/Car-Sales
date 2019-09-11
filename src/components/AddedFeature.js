a5dd705ac05fe65fce35ac0c348ea6ddc3c968d1
Jonathan Ho <johnnyho82@gmail.com>
2019-09-11 19:15:02 -0400

    Experiment with Bulma styles

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -5,9 +5,9 @@ const AddedFeature = props => {
     <li>
       {/* Add an onClick to run a function to remove a feature */}
       <button
-        className='button'
+        className='button2'
         onClick={() => props.handleRemoveFeature(props.feature.id)}>
-        X
+        <a class='delete'></a>
       </button>
       {props.feature.name}
     </li>
