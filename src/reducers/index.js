573387e61ea76ee8f6645e04e5fbe774f2d73cda
Jonathan Ho <johnnyho82@gmail.com>
2019-09-11 15:54:29 -0400

    Add add feature functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { ADD_FEATURE } from '../actions';
+
 const initialState = {
   additionalPrice: 0,
   car: {
@@ -17,6 +19,22 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_FEATURE:
+      console.log(action.payload);
+      return {
+        ...state,
+        additionalPrice:
+          state.additionalPrice +
+          state.store.find(item => item.id === action.payload).price,
+        car: {
+          ...state.car,
+          features: [
+            ...state.car.features,
+            state.store.find(item => item.id === action.payload),
+          ],
+        },
+        store: state.store.filter(item => item.id !== action.payload),
+      };
     default:
       return state;
   }
